3f1cf374c7851678467cda2af2c4efc4f9822912
Solidhan5000 <hasanhan5000@gmail.com>
2013-04-16 22:15:40 +0200

    Finish the static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,13 +1,70 @@
 require 'spec_helper'
 
+# Integration testing on page content
 describe 'StaticPages' do
 
   describe 'Home Page' do
-    it "should have the content 'Sample App'" do
-
+    # A behaviour test to check whether h1 content is displayed as expected
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    # A behaviour test to check if the dynamically created title matches what is expected
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
+
+
+  describe 'Help Page' do
+    # A behaviour test to check whether h1 content is displayed as expected
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    # A behaviour test to check if the dynamically created title matches what is expected
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+
+
+
+  describe 'About Page' do
+    # A behaviour test to check whether h1 content is displayed as expected
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    # A behaviour test to check if the dynamically created title matches what is expected
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+
+  describe 'Contact Page' do
+    # A behaviour test to check whether h1 content is displayed as expected
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    # A behaviour test to check if the dynamically created title matches what is expected
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+
+  end
 end
