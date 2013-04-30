4bf35525e85e78b3a041d2efe44e4b88ffbca1ad
Solidhan5000 <hasanhan5000@gmail.com>
2013-04-30 19:29:02 +0200

    Create static pages layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 TutorialSampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  root :to => 'static_pages#home'                   # the standard root route
+
+  match '/help',    to: 'static_pages#help'     # automatically creates: help_path
+  match '/about',   to: 'static_pages#about'    # automatically creates: about_path
+  match '/contact', to: 'static_pages#contact'  # automatically creates: contact
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,74 +3,38 @@ require 'spec_helper'
 # Integration testing on page content
 describe 'StaticPages' do
 
+  subject { page }  # sets the test environment for the tests in this block
+
   describe 'Home Page' do
-    # A behaviour test to check whether h1 content is displayed as expected
-    it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
-    end
+    before { visit root_path } # this will run this line before each of the other tests
 
-    # A behaviour test to check if the dynamically created title matches what is expected
-    it "should have the base title only" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App")
-    end
+    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: "| Home") }
 
-    # A behaviour test to check that the home page does not have a added page_title
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text=>"| Home")
-    end
   end
-
-
 
   describe 'Help Page' do
-    # A behaviour test to check whether h1 content is displayed as expected
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-    end
+    before { visit help_path }
 
-    # A behaviour test to check if the dynamically created title matches what is expected
-    it "should have the right title" do
-      visit '/static_pages/help'
-      page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | Help")
-    end
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
+
   end
-
-
 
   describe 'About Page' do
-    # A behaviour test to check whether h1 content is displayed as expected
-    it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
-    end
+    before { visit about_path }
 
-    # A behaviour test to check if the dynamically created title matches what is expected
-    it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | About Us")
-    end
+    it { should have_selector('h1', text: 'About Us') }
+    it { should have_selector('title', text: full_title('About Us')) }
+
   end
 
-
   describe 'Contact Page' do
-    # A behaviour test to check whether h1 content is displayed as expected
-    it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact')
-    end
+    before { visit contact_path }
 
-    # A behaviour test to check if the dynamically created title matches what is expected
-    it "should have the right title" do
-      visit '/static_pages/contact'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
-    end
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
 
   end
 end
